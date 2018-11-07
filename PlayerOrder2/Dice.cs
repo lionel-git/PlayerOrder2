41c54147de021ddf9c814402fd87f6a320eaaa37
Lionel Desorme <lionel.desorme@free.fr>
2018-11-07 17:45:08 +0100

    init commit

---
+++ PlayerOrder2/Dice.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerOrder2
+{
+    public class Dice
+    {
+        private static int _faces;
+        private string _name;
+        private static Random _rand = new Random(42);
+
+        double[] _probability;
+
+        public static void SetFaces(int faces)
+        {
+            _faces = faces;
+        }
+
+        public Dice(string name)
+        {
+            _name = name;
+            _probability = new double[_faces];
+            
+            double sum = 0;
+            for (int i = 0; i < _faces; i++)
+            {
+                _probability[i] = _rand.NextDouble();
+                sum += _probability[i];
+            }
+            for (int i = 0; i < _faces; i++)
+                _probability[i] /= sum;
+        }
+
+        public double ProbaVictory(Dice B)
+        {
+            double proba = 0.0;
+            for (int i = 0; i < _faces; i++)
+            {
+                for (int j = i+1; j < _faces; j++)
+                {
+                    // On a j > i
+                    // This tire j et B tire i => victoire
+                    // This tire i et B tire j => defaite
+                    // Si i = j => nulle
+                    proba += _probability[j] * B._probability[i] - _probability[i] * B._probability[j];
+                }
+            }
+            return proba;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"{_name}: ");
+            for (int i = 0; i < _faces; i++)
+                sb.Append($"{_probability[i]:0.00} ");
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ PlayerOrder2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerOrder2
+{
+    class Program
+    {
+        const double minProba = 0.05;
+
+        static void SearchOrder(int N, int n)
+        {
+            double probaMax = -1;
+            object mutexProbaMax = new object();
+
+            for (int i = 0; i < N; i++)
+            {
+                var diceA = new Dice("A");
+                var diceB = new Dice("B");
+                double pAB = diceA.ProbaVictory(diceB);
+                if (Math.Abs(pAB) > minProba)
+                {
+                    if (pAB < 0)
+                    {
+                        var tmp = diceA;
+                        diceA = diceB;
+                        diceB = tmp;
+                        pAB = -pAB; // pAB>0
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        var diceC = new Dice("C");
+                        double pBC = diceB.ProbaVictory(diceC);
+                        double pCA = diceC.ProbaVictory(diceA);
+                        if (pBC > minProba && pCA > minProba)
+                        {
+                            double s = pAB + pBC + pCA;
+                            if (s > probaMax)
+                            {
+                                Console.WriteLine(diceA);
+                                Console.WriteLine(diceB);
+                                Console.WriteLine(diceC);
+                                Console.WriteLine($"A>B: {pAB}");
+                                Console.WriteLine($"B>C: {pBC}");
+                                Console.WriteLine($"C>A: {pCA}");
+                                probaMax = s;
+                                Console.WriteLine($"Sum proba: {probaMax}");
+                            }
+                        }
+                    }
+                }
+                if (i % 1000 == 0)
+                    Console.WriteLine($"i={i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+            try
+            {
+                Dice.SetFaces(4);
+                SearchOrder(10000, 100000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
